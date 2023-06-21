20947eb04d4d5d0a78ead22dcb9b04f051473aa4
DanilaMM <vip.masychev@mail.ru>
2023-06-21 18:22:18 +0300

    project version 1.0
    build(create autorization): 1231312312
    
    Создал авторизацию, не на основе классов
    
    !Не на основе классов, страница в книге 199

--- account/apps.py
+++ account/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class SocsiteConfig(AppConfig):
+class AccountConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'socsite'
+    name = 'account'
