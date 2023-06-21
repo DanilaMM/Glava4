c939a56005addabf64dd89c14e824e4e64cbd2fd
DanilaMM <vip.masychev@mail.ru>
2023-06-21 18:46:03 +0300

    project version 1.1
    refactor(refactor system autorization): change autorication
    
    Изменил авторизацию в аккаунт на django классы , встроенные в библиотеку
    
    !тут должен быть футер

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('login/', views.user_login, name='login'),
+    # path('login/', views.user_login, name='login'), старый способ без классов
+    path('login/', auth_views.LoginView.as_view(), name='login'), #вход в акк
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'), #выход с акка
+    path('', views.dashboard, name='dashboard'), # вход в доску
 ]
